0f4c958ea6c933517fb1c221484d8c511bf4f49f
napo <napo@fbk.eu>
2014-12-30 01:55:35 +0100

    update

--- download_civici_umbria.sh
+++ download_civici_umbria.sh
@@ -3,7 +3,7 @@ prev="1"
 url1="http://geo.umbriaterritorio.it/ArcGIS/rest/services/Public/ECOGRAFICO_CATASTALE1_WGS84/MapServer/0/query?geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=+OBJECTID%3E%3D"
 url2="+and+OBJECTID%3C%3D"
 url3="&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=*&f=pjson"
-for i in `seq 1000 1000 200000`
+for i in `seq 1000 1000 192000`
 do
 	url=$url1$prev$url2$i$url3
 	echo "generate shapefile from record $prev to record $i"
